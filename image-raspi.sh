e3f86d61c1deb9976e76d9c9f42429c6dade6e12
Monica <wolfmurdurer23@gmail.com>
2021-11-17 22:52:33 -0600

    Started working on multiple destinations, currently only one disk at a time.

--- image-raspi.sh
+++ image-raspi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 failsafe='I have confirmed all information is correct and understand the risks'
-#failsafe='check'
 processInput=''
 user=''
 
@@ -11,17 +10,23 @@ then
     exit
 fi
 
-# Disclaimer
-clear
-echo "This program, when used improperly, can irreversably destroy all data on your computer."
-echo "This program contains absolutely no warranty where permissible by law."
-echo "By continuing to use this program, you consent to the Terms of Service of this program and understand the risks."
-echo "If you do not consent to the Terms of Service, please close the program now."
-echo "This disclaimer will show for 10 seconds, press any key to continue"
+if [[ $1 != "development" ]]
+then
+    # Disclaimer
+    clear
+    echo "This program, when used improperly, can irreversably destroy all data on your computer."
+    echo "This program contains absolutely no warranty where permissible by law."
+    echo "By continuing to use this program, you consent to the Terms of Service of this program and understand the risks."
+    echo "If you do not consent to the Terms of Service, please close the program now."
+    echo "This disclaimer will show for 10 seconds, press any key to continue"
 
-# Sleep only commented out for testing
-sleep 10
-pause
+    # Sleep only commented out for testing
+    sleep 10
+    pause
+elif [[ $1 == "development" ]]
+then
+    failsafe = 'check'
+fi
 
 # Commented out because mass imaging isn't quite ready
 #echo "Would you like to Create an image, Restore an image, or Mass-restore images?"
@@ -119,9 +124,12 @@ then
     read imageConfirmation
 
     # If the user successfully passes the failsafe, then create an image
+    # Unsure if unmounting is necessary, might as well have it just in case
     if [[ $imageConfirmation == $failsafe ]]; then
         echo "Imaging from " ~/images/$imagePath " to " /dev/$devName
+        umount /dev/$devName* -f -l
         dd if=~/images/$imagePath of=/dev/$devName status=progress
+
     # If the user fails the failsafe, bail
     else
         echo "Confirmation failed, exiting"
@@ -137,21 +145,63 @@ then
     # Totally not me being lazy and just not wanting to comment out the elif statement
     echo "Multiple imaging isn't quite ready! If you feel confident, I encourage you to create a pull request or an issue with suggestions on github."
     echo "Even though you likely got this from github, you can create a pull request at https://github.com/TotallyMonica/image-raspi"
+    echo "Currently, only one destination is supported"
     echo "- Monica"
-    exit
 
     # Get number of drives imaging to
     echo "How many drives are you imaging"
     read driveCount
 
-    # Get number of physical drives to image at once
     # TODO: Clean up grammar, make it easier to read
-    echo "How many physical drives can you connect at once?"
-    read diskCount
+    fdisk -l
+    echo ""
+    echo "Please type in each device name, then type 'complete' when done"
+    declare -a physicalDiskPath
+    while read diskPath
+    do
+        i=i+1
+        read physicalDiskPath=$("${diskPath[@]}")
+        if [[ diskPath[$i]==complete ]]
+        then
+            physicalDiskPath=("${array[@]/complete}")
+            break
+        fi
+    done
+
+    # TODO: add support for multiple destination disks. Currently, only one disk, so overriding user's input.
+    diskCount = ${#physicalDiskPath[@]}
+    echo "We counted "$diskCount" disks"
 
     # List disks and get physical disk location
     fdisk -l
-    #for (( i=1; i<=diskCount; i++ ))
+    
+    for i in $diskCount
+    do
+        echo "You're imaging disk /dev/$physicalDiskPath[$i]"
+    done
+
+    if [[ $diskCount == 1 ]]
+    then
+        clear
+        echo ""
+        echo "The image " ~/images/$imagePath " and flashed to /dev/$devName"
+        echo "Please ensure everything is correct, as this process is potentially dangerous."
+        echo "Type in 'I have confirmed all information is correct and understand the risks' to proceed"
+        read imageConfirmation
+        for i in $diskCount
+        do
+            dd if=~/images/$imagePath of=/dev/$devName status=progress
+        done
+
+    elif [[ $diskCount != 1 ]]
+    then
+        echo "Drive " "will be written to first, the rest will be queued up"
+        imageCount = 1
+        while driveCount < imageCount
+        do
+            dd if=~/image/imagePath of=/dev/$devName status=progress
+        done
+    fi
 
 # If any of the other commands didn't run, 
 else
